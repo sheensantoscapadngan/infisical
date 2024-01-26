48e95a401c5563c21fc2d6ff0cb6b6c93581b4f7
Sheen Capadngan <sheen@symph.co>
2024-01-27 02:32:13 +0800

    developed initial create send form

---
+++ frontend/src/views/SendSecret/components/CreateSendSecretForm.tsx
@@ -0,0 +1,82 @@
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, FormControl, Input, Modal, ModalContent, SecretInput } from "@app/components/v2";
+
+const typeSchema = z.object({
+    key: z.string(),
+    value: z.string().optional()
+  });
+  
+type TFormSchema = z.infer<typeof typeSchema>;
+
+export const CreateSendSecretForm = () => {
+    const {
+        register,
+        handleSubmit,
+        control,
+        reset,
+        formState: { errors, isSubmitting }
+    } = useForm<TFormSchema>({ resolver: zodResolver(typeSchema) });
+
+    const handleFormSubmit = async ({ key, value }: TFormSchema) => {
+        try {
+         
+        } catch (error) {
+
+        }
+    };
+
+    return (
+        <Modal
+          isOpen={true}
+        >
+          <ModalContent
+            title="Add secret"
+            subTitle="Create a secret for sharing"
+          >
+            <form onSubmit={handleSubmit(handleFormSubmit)}>
+              <FormControl label="Key" isError={Boolean(errors?.key)} errorText={errors?.key?.message}>
+                <Input
+                  placeholder="Type your secret name"
+                />
+              </FormControl>
+              <Controller
+                control={control}
+                name="value"
+                render={({ field }) => (
+                  <FormControl
+                    label="Value"
+                    isError={Boolean(errors?.value)}
+                    errorText={errors?.value?.message}
+                  >
+                    <SecretInput
+                      {...field}
+                      containerClassName="text-bunker-300 hover:border-primary-400/50 border border-mineshaft-600 bg-mineshaft-900 px-2 py-1.5"
+                    />
+                  </FormControl>
+                )}
+              />
+              <div className="mt-7 flex items-center">
+                <Button
+                  isDisabled={isSubmitting}
+                  isLoading={isSubmitting}
+                  key="layout-create-project-submit"
+                  className="mr-4"
+                  type="submit"
+                >
+                  Create Send
+                </Button>
+                <Button
+                  key="layout-cancel-create-project"
+                  variant="plain"
+                  colorSchema="secondary"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </ModalContent>
+        </Modal>
+      );
+}
