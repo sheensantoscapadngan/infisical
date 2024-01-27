a3ea31bd210d3a366f777c1c81fa7db159e6f84b
Sheen Capadngan <sheen@symph.co>
2024-01-27 10:06:19 +0800

    lint newly created components

--- frontend/src/pages/send-secret/index.tsx
+++ frontend/src/pages/send-secret/index.tsx
@@ -8,67 +8,66 @@ import { Button, Input } from "@app/components/v2";
 import { CreateSendSecretForm } from "@app/views/SendSecret/components/CreateSendSecretForm";
 
 export default function SendSecret() {
-    const { t } = useTranslation();
-    const [isAddModalOpen, setAddModalState] = useState(false);
+  const { t } = useTranslation();
+  const [isAddModalOpen, setAddModalState] = useState(false);
 
-    return (
-        <>
-            <Head>
-                <title>{t("common.head-title", { title: t("settings.org.title") })}</title>
-                <link rel="icon" href="/infisical.ico" />
-            </Head>
-            <div className="container mx-auto flex flex-col justify-between bg-bunker-800 text-white">
-                <div className="mb-6 w-full py-6 px-6 max-w-7xl mx-auto">
-                    <p className="mr-4 mb-4 text-3xl font-semibold text-white">
-                        Send Secrets
-                    </p>
-                    <div className="mb-6 text-lg text-mineshaft-300">
-                        A secure and efficient way to share text-based sensitive information with intended recipients
-                    </div>
-                    <div className="flex items-center space-x-2 mt-4">
-                        <div className="w-2/5">
-                            <Input
-                                className="bg-mineshaft-800 placeholder-mineshaft-50 duration-200 focus:bg-mineshaft-700/80"
-                                placeholder="Search by folder name, key name, comment..."
-                                leftIcon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
-                            />
-                        </div>
-                        <div className="flex-grow"/>
-                        <div className="flex items-center mr-20">
-                            <Button
-                                variant="outline_bg"
-                                leftIcon={<FontAwesomeIcon icon={faPlus} />}
-                                className="rounded-r-none h-10"
-                                onClick={() => setAddModalState(true)}
-                            >
-                                Add Secret
-                            </Button>
-                        </div>
-                    </div>
-                    <div className="mt-3 overflow-y-auto overflow-x-hidden thin-scrollbar bg-mineshaft-800 text-left text-bunker-300 rounded-md text-sm">
-                        <div className="flex flex-col" id="dashboard">
-                            <div className="flex font-medium border-b border-mineshaft-600">
-                                <div style={{ width: "2.8rem" }} className="px-4 py-3 flex-shrink-0" />
-                                <div
-                                className="w-80 flex-shrink-0 border-r flex items-center border-mineshaft-600 px-4 py-2"
-                                role="button"
-                                tabIndex={0}
-                                >
-                                Key
-                                <FontAwesomeIcon
-                                    icon={faArrowUp}
-                                    className="ml-2"
-                                />
-                                </div>
-                            <div className="flex-grow px-4 py-2">Value</div>
-                            </div>      
-                        </div>
-                    </div>
-                    <CreateSendSecretForm isAddModalOpen={isAddModalOpen} setAddModalState={setAddModalState}/>
-                </div>
+  return (
+    <>
+      <Head>
+        <title>{t("common.head-title", { title: t("settings.org.title") })}</title>
+        <link rel="icon" href="/infisical.ico" />
+      </Head>
+      <div className="container mx-auto flex flex-col justify-between bg-bunker-800 text-white">
+        <div className="mx-auto mb-6 w-full max-w-7xl py-6 px-6">
+          <p className="mr-4 mb-4 text-3xl font-semibold text-white">Send Secrets</p>
+          <div className="mb-6 text-lg text-mineshaft-300">
+            A secure and efficient way to share text-based sensitive information with intended
+            recipients
+          </div>
+          <div className="mt-4 flex items-center space-x-2">
+            <div className="w-2/5">
+              <Input
+                className="bg-mineshaft-800 placeholder-mineshaft-50 duration-200 focus:bg-mineshaft-700/80"
+                placeholder="Search by folder name, key name, comment..."
+                leftIcon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+              />
             </div>
-        </>
-    );
+            <div className="flex-grow" />
+            <div className="mr-20 flex items-center">
+              <Button
+                variant="outline_bg"
+                leftIcon={<FontAwesomeIcon icon={faPlus} />}
+                className="h-10 rounded-r-none"
+                onClick={() => setAddModalState(true)}
+              >
+                Add Secret
+              </Button>
+            </div>
+          </div>
+          <div className="thin-scrollbar mt-3 overflow-y-auto overflow-x-hidden rounded-md bg-mineshaft-800 text-left text-sm text-bunker-300">
+            <div className="flex flex-col" id="dashboard">
+              <div className="flex border-b border-mineshaft-600 font-medium">
+                <div style={{ width: "2.8rem" }} className="flex-shrink-0 px-4 py-3" />
+                <div
+                  className="flex w-80 flex-shrink-0 items-center border-r border-mineshaft-600 px-4 py-2"
+                  role="button"
+                  tabIndex={0}
+                >
+                  Key
+                  <FontAwesomeIcon icon={faArrowUp} className="ml-2" />
+                </div>
+                <div className="flex-grow px-4 py-2">Value</div>
+              </div>
+            </div>
+          </div>
+          <CreateSendSecretForm
+            isAddModalOpen={isAddModalOpen}
+            setAddModalState={setAddModalState}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 Object.assign(SendSecret, { requireAuth: true });
--- frontend/src/views/SendSecret/components/CreateSendSecretForm.tsx
+++ frontend/src/views/SendSecret/components/CreateSendSecretForm.tsx
@@ -1,7 +1,16 @@
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, FormControl, Input, Modal, ModalContent, SecretInput, Select, SelectItem } from "@app/components/v2";
+import {
+  Button,
+  FormControl,
+  Input,
+  Modal,
+  ModalContent,
+  SecretInput,
+  Select,
+  SelectItem
+} from "@app/components/v2";
 
 const expirations = [
   { label: "1 hour", value: "1h" },
@@ -12,11 +21,20 @@ const expirations = [
   { label: "30 days", value: "30d" }
 ];
 
+const expirationMapping: { [key: string]: number } = {
+  "1h": 3600,
+  "1d": 86400,
+  "2d": 172800,
+  "3d": 259200,
+  "7d": 604800
+};
+
 const typeSchema = z.object({
-    key: z.string(),
-    value: z.string().optional()
-  });
-  
+  key: z.string(),
+  value: z.string(),
+  expiresIn: z.string()
+});
+
 type TFormSchema = z.infer<typeof typeSchema>;
 
 type Props = {
@@ -25,102 +43,87 @@ type Props = {
 };
 
 export const CreateSendSecretForm = (props: Props) => {
-    const { isAddModalOpen, setAddModalState } = props;
-    const {
-        register,
-        handleSubmit,
-        control,
-        reset,
-        formState: { errors, isSubmitting }
-    } = useForm<TFormSchema>({ resolver: zodResolver(typeSchema) });
+  const { isAddModalOpen, setAddModalState } = props;
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isSubmitting }
+  } = useForm<TFormSchema>({ resolver: zodResolver(typeSchema) });
 
-    const handleFormSubmit = async ({ key, value }: TFormSchema) => {
-        try {
-         
-        } catch (error) {
+  const handleFormSubmit = async ({ key, value }: TFormSchema) => {
+    try {
+    } catch (error) {}
+  };
 
-        }
-    };
-
-    return (
-        <Modal
-          isOpen={isAddModalOpen}
-          onOpenChange={(state) => setAddModalState(state)}
-        >
-          <ModalContent
-            title="Add secret"
-            subTitle="Create a secret for sharing"
-          >
-            <form onSubmit={handleSubmit(handleFormSubmit)}>
-              <FormControl label="Key" isError={Boolean(errors?.key)} errorText={errors?.key?.message}>
-                <Input
-                  placeholder="Type your secret name"
+  return (
+    <Modal isOpen={isAddModalOpen} onOpenChange={(state) => setAddModalState(state)}>
+      <ModalContent title="Add secret" subTitle="Create a secret for sharing">
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <FormControl label="Key" isError={Boolean(errors?.key)} errorText={errors?.key?.message}>
+            <Input placeholder="Type your secret name" />
+          </FormControl>
+          <Controller
+            control={control}
+            name="value"
+            render={({ field }) => (
+              <FormControl
+                label="Value"
+                isError={Boolean(errors?.value)}
+                errorText={errors?.value?.message}
+              >
+                <SecretInput
+                  {...field}
+                  containerClassName="text-bunker-300 hover:border-primary-400/50 border border-mineshaft-600 bg-mineshaft-900 px-2 py-1.5"
                 />
               </FormControl>
-              <Controller
-                control={control}
-                name="value"
-                render={({ field }) => (
-                  <FormControl
-                    label="Value"
-                    isError={Boolean(errors?.value)}
-                    errorText={errors?.value?.message}
-                  >
-                    <SecretInput
-                      {...field}
-                      containerClassName="text-bunker-300 hover:border-primary-400/50 border border-mineshaft-600 bg-mineshaft-900 px-2 py-1.5"
-                    />
-                  </FormControl>
-                )}
-              />
+            )}
+          />
 
-              <Controller
-                control={control}
-                name="expiresIn"
-                defaultValue="1h"
-                render={({ field: { onChange, ...field }, fieldState: { error } }) => (
-                    <FormControl
-                        label="Expiration"
-                        errorText={error?.message}
-                        isError={Boolean(error)}
-                    >
-                        <Select
-                            defaultValue={field.value}
-                            {...field}
-                            onValueChange={(e) => onChange(e)}
-                            className="w-full"
-                        >
-                            {expirations.map(({ label, value }) => (
-                                <SelectItem value={String(value || "")} key={`api-key-expiration-${label}`}>
-                                    {label}
-                                </SelectItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                )}
-              />
+          <Controller
+            control={control}
+            name="expiresIn"
+            defaultValue="1h"
+            render={({ field: { onChange, ...field }, fieldState: { error } }) => (
+              <FormControl label="Expiration" errorText={error?.message} isError={Boolean(error)}>
+                <Select
+                  defaultValue={field.value}
+                  {...field}
+                  onValueChange={(e) => onChange(e)}
+                  className="w-full"
+                >
+                  {expirations.map(({ label, value }) => (
+                    <SelectItem value={String(value || "")} key={`api-key-expiration-${label}`}>
+                      {label}
+                    </SelectItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />
 
-              <div className="mt-7 flex items-center">
-                <Button
-                  isDisabled={isSubmitting}
-                  isLoading={isSubmitting}
-                  key="layout-create-project-submit"
-                  className="mr-4"
-                  type="submit"
-                >
-                  Create Send
-                </Button>
-                <Button
-                  key="layout-cancel-create-project"
-                  variant="plain"
-                  colorSchema="secondary"
-                  onClick={() => setAddModalState(false)}
-                >
-                  Cancel
-                </Button>
-              </div>
-            </form>
-          </ModalContent>
-        </Modal>
-      );
-}
+          <div className="mt-7 flex items-center">
+            <Button
+              isDisabled={isSubmitting}
+              isLoading={isSubmitting}
+              key="layout-create-project-submit"
+              className="mr-4"
+              type="submit"
+            >
+              Create Send
+            </Button>
+            <Button
+              key="layout-cancel-create-project"
+              variant="plain"
+              colorSchema="secondary"
+              onClick={() => setAddModalState(false)}
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </ModalContent>
+    </Modal>
+  );
+};
