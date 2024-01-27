9c75b37443b050512b77539ee218d6e69df6d356
Sheen Capadngan <sheen@symph.co>
2024-01-27 20:30:39 +0800

    added auto expiry for send secret documents

--- backend/src/models/sendSecret.ts
+++ backend/src/models/sendSecret.ts
@@ -14,56 +14,58 @@ export interface ISendSecret {
   user: Types.ObjectId;
 }
 
-const sendSecretSchema = new Schema<ISendSecret>({
-  encryptionKeyCiphertext: {
-    type: String,
-    required: true
+const sendSecretSchema = new Schema<ISendSecret>(
+  {
+    encryptionKeyCiphertext: {
+      type: String,
+      required: true
+    },
+    encryptionKeyIV: {
+      type: String,
+      required: true
+    },
+    encryptionKeyTag: {
+      type: String,
+      required: true
+    },
+    expiresAt: {
+      type: Date,
+      required: true
+    },
+    secretKeyCiphertext: {
+      type: String,
+      required: true
+    },
+    secretKeyIV: {
+      type: String,
+      required: true
+    },
+    secretKeyTag: {
+      type: String,
+      required: true
+    },
+    secretValueCiphertext: {
+      type: String,
+      required: true
+    },
+    secretValueIV: {
+      type: String,
+      required: true
+    },
+    secretValueTag: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
   },
-  encryptionKeyIV: {
-    type: String,
-    required: true
-  },
-  encryptionKeyTag: {
-    type: String,
-    required: true
-  },
-  expiresAt: {
-    type: Date,
-    required: true
-  },
-  secretKeyCiphertext: {
-    type: String,
-    required: true
-  },
-  secretKeyIV: {
-    type: String,
-    required: true
-  },
-  secretKeyTag: {
-    type: String,
-    required: true
-  },
-  secretValueCiphertext: {
-    type: String,
-    required: true
-  },
-  secretValueIV: {
-    type: String,
-    required: true
-  },
-  secretValueTag: {
-    type: String,
-    required: true
-  },
-  user: {
-    // user associated with the send secret
-    type: Schema.Types.ObjectId,
-    ref: "User"
+  {
+    timestamps: true
   }
-});
+);
 
-sendSecretSchema.index({
-  user: 1
-});
+sendSecretSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 20 });
 
 export const SendSecret = model<ISendSecret>("SendSecret", sendSecretSchema);
